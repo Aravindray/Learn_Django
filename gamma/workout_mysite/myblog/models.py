a5e8f6acfc9cd3ad387846ea545f5c5089a8294d
Aravindray <aravindselvasan@gmail.com>
2024-07-23 13:11:32 +0530

    added enum field using models.TextChoices class inside the Post class and added a field status with status = models.CharField(max_length=2, choices = Status.choices, default = Status.DRAFT

--- gamma/workout_mysite/myblog/models.py
+++ gamma/workout_mysite/myblog/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft',
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -10,11 +15,14 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now())
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
         indexes = [
-            models.Index(fields=['publish'])
+            models.Index(fields=['-publish'])
         ]
 
     def __str__(self):
