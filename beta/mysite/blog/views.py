aad9d7fb417226abc1fa37cae227d98db6d44956
Aravindray <aravindselvasan@gmail.com>
2024-07-24 19:03:56 +0530

    modified post_detail() function parameters, based on new url pattern

--- beta/mysite/blog/views.py
+++ beta/mysite/blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+# from django.http import Http404
 
 def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html',{'posts': posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status = Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status = Post.Status.PUBLISHED,
+                             publish__year = year,
+                             publish__month = month,
+                             publish__day = day,
+                             slug = post)
     
     return render(request, 'blog/post/detail.html', {'post': post})
