0966deab2c69c5fcbd8f336a8c744a8e9ca8ed2c
Aravindray <aravindselvasan@gmail.com>
2024-07-28 20:22:10 +0530

    learning about recommended posts

--- beta/mysite/blog/views.py
+++ beta/mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -32,10 +33,19 @@ def post_detail(request, year, month, day, post):
                              slug = post)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in = post_tags_ids)\
+                    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags = Count('tags'))\
+                                 .order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED)
